150cfad65c59d586ddaca12c9ddec3db63522094
bayuTri-Code <bayurara555@gmail.com>
2025-12-08 14:11:35 +0700

    add: add min and max size file biodata

--- internal/config/minIO.go
+++ internal/config/minIO.go
@@ -13,6 +13,8 @@ type MinioConfiguration struct {
 	Bucket        string
 	UseSSL        bool
 	ImageENDPOINT string
+	MinSize       int64
+	MaxSize       int64
 }
 
 var MinioConfig *MinioConfiguration
@@ -25,10 +27,14 @@ func InitMinioConfig() {
 		Bucket:        viper.GetString("MINIO_BUCKET"),
 		UseSSL:        viper.GetBool("MINIO_USE_SSL"),
 		ImageENDPOINT: viper.GetString("IMAGE_ENDPOINT"),
+		MinSize:       viper.GetInt64("MINIO_MIN_UPLOAD_SIZE"),
+		MaxSize:       viper.GetInt64("MINIO_MAX_UPLOAD_SIZE"),
 	}
 
 	log.Println("[Config] MinIO configuration loaded successfully")
 	log.Printf("[Config] MinIO Endpoint: %s", MinioConfig.Endpoint)
 	log.Printf("[Config] MinIO Bucket: %s", MinioConfig.Bucket)
 	log.Printf("[Config] MinIO UseSSL: %v", MinioConfig.UseSSL)
+	log.Printf("[Config] MinIO MinSize: %d bytes", MinioConfig.MinSize)
+	log.Printf("[Config] MinIO MaxSize: %d bytes", MinioConfig.MaxSize)
 }
