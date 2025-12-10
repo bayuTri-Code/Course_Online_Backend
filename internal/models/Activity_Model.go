09368b507ce49646a49b9a2198042458c158c6f0
bayuTri-Code <bayurara555@gmail.com>
2025-12-10 15:56:14 +0700

    add: add activity feature for question management

--- internal/models/Activity_Model.go
+++ internal/models/Activity_Model.go
@@ -8,7 +8,7 @@ import (
 
 type Activity struct {
 	ID           uuid.UUID `gorm:"type:uuid;default:gen_random_uuid();primaryKey" json:"id"`
-	ActivityName string    `gorm:"size:100;not null" json:"activity_name"`
+	ActivityName string    `gorm:"size:255;not null" json:"activity_name"`
 	UserID       uuid.UUID `gorm:"type:uuid;not null;index" json:"user_id"`
 	When         time.Time `gorm:"not null" json:"when"`
 
