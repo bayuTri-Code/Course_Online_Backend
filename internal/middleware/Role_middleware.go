c51910337e0cca4e70ee5867b52198095a0db90a
bayuTri-Code <bayurara555@gmail.com>
2025-12-16 23:35:03 +0700

    update: update middleware

--- internal/middleware/Role_middleware.go
+++ internal/middleware/Role_middleware.go
@@ -6,7 +6,6 @@ import (
 
 	"github.com/gin-gonic/gin"
 	
-	"gorm.io/gorm"
 )
 
 func RoleMiddleware(allowedRoles ...string) gin.HandlerFunc {
@@ -58,95 +57,3 @@ func RoleMiddleware(allowedRoles ...string) gin.HandlerFunc {
 	}
 }
 
-func CheckCourseOwnershipDynamic(db *gorm.DB) gin.HandlerFunc {
-	return func(c *gin.Context) {
-		userID, _ := c.Get("user_id")
-		rolesValue, _ := c.Get("roles")
-		userIDStr := userID.(string)
-
-		isSuper := false
-		isAdmin := false
-
-		switch r := rolesValue.(type) {
-		case string:
-			if r == "super_admin" {
-				isSuper = true
-			}
-			if r == "admin" {
-				isAdmin = true
-			}
-		case []string:
-			for _, role := range r {
-				if role == "super_admin" {
-					isSuper = true
-				}
-				if role == "admin" {
-					isAdmin = true
-				}
-			}
-		}
-
-		if isSuper {
-			c.Next()
-			return
-		}
-
-		var courseID string
-
-		if cid := c.Param("id"); cid != "" {
-			courseID = cid
-		}
-
-		//quiz
-		if qid := c.Param("quizId"); qid != "" {
-			_ = db.Raw(`
-        SELECT course_id FROM quizzes WHERE id = ?
-    `, qid).Scan(&courseID)
-		}
-
-		if mid := c.Param("module_id"); mid != "" {
-			_ = db.Raw(`
-                SELECT course_id FROM modules WHERE id = ?
-            `, mid).Scan(&courseID)
-		}
-
-		if lid := c.Param("id"); lid != "" && c.FullPath() != "/courses/:id" {
-			_ = db.Raw(`
-                SELECT m.course_id 
-                FROM lessons l 
-                JOIN modules m ON l.module_id = m.id
-                WHERE l.id = ?
-            `, lid).Scan(&courseID)
-		}
-
-		if courseID == "" {
-			c.JSON(400, gin.H{"message": "Invalid or missing resource identifier"})
-			c.Abort()
-			return
-		}
-
-		var createdBy string
-		err := db.Raw(`
-            SELECT created_by FROM courses WHERE id = ?
-        `, courseID).Scan(&createdBy).Error
-
-		if err != nil || createdBy == "" {
-			c.JSON(404, gin.H{"message": "Course not found"})
-			c.Abort()
-			return
-		}
-
-		if isAdmin {
-			if createdBy == userIDStr {
-				c.Next()
-				return
-			}
-			c.JSON(403, gin.H{"message": "Forbidden: you can only manage your own course content"})
-			c.Abort()
-			return
-		}
-
-		c.JSON(403, gin.H{"message": "Forbidden: insufficient permissions"})
-		c.Abort()
-	}
-}
